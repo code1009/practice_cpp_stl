b530449dc527d45994a78971d94158c0729f0186
code1009 <code1009@naver.com>
2024-09-26 16:34:25 +0900

    1

--- practice_cpp_stl/src/main.cpp
+++ practice_cpp_stl/src/main.cpp
@@ -42,15 +42,55 @@
 
 /////////////////////////////////////////////////////////////////////////////
 //===========================================================================
+void test_utf8(void)
+{
+	// vld test
+	char* p = new char{ 0 };
+
+	OutputDebugStringA("\r\n");
+	OutputDebugStringA("\r\n");
+	OutputDebugStringA("\r\n");
+	OutputDebugStringA((LPCSTR)u8"MESSAGE:");
+	OutputDebugStringA((LPCSTR)u8"가나다라"); // 출력 안됨
+	OutputDebugStringA("\r\n");
+	OutputDebugStringA("\r\n");
+	OutputDebugStringA("\r\n");
+
+	char8_t A_ga_B[6] = { 65, 234, 176, 128, 66, 0 };
+
+	MessageBoxA(nullptr, (LPCSTR)u8"가나다라", (LPCSTR)A_ga_B, MB_OK);
+	OutputDebugStringA((LPCSTR)A_ga_B); // 출력 안됨
+	
+#if 0
+<?xml version="1.0" encoding="utf-8" standalone="yes"?>
+<assembly manifestVersion="1.0" xmlns="urn:schemas-microsoft-com:asm.v1">
+  <assemblyIdentity type="win32" name="..." version="6.0.0.0"/>
+  <application>
+    <windowsSettings>
+      <activeCodePage xmlns="http://schemas.microsoft.com/SMI/2019/WindowsSettings">UTF-8</activeCodePage>
+    </windowsSettings>
+  </application>
+</assembly>
+#endif
+}
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+//===========================================================================
 int APIENTRY wWinMain(
 	HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
 	LPWSTR    lpCmdLine,
 	int       nCmdShow)
 {
-	char* p = new char{ 0 };
 
-	MessageBoxA(nullptr, (LPCSTR)u8"가나다라", (LPCSTR)u8"제목", MB_OK);
+	char* p = new char[1];
+
+	test_utf8();
+
 
 	return 0;
 }
